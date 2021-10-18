e2c45c0ae785b74dd367eed77702b758e3575140
KimTaeHee <rlaxogmldhkd@gmail.com>
2021-10-18 16:19:24 +0900

    aaaaa bbbb

--- app/src/main/java/com/example/everylive/login/b.java
+++ app/src/main/java/com/example/everylive/login/b.java
@@ -3,4 +3,16 @@ package com.example.everylive.login;
 public class b {
 
     /// 수미
+
+    /**
+     * 인텐트로 액티비티 이동 및 데이터 송신
+     */
+            Intent intent = new Intent(getApplicationContext(), .class);
+            startActivity(intent);
+            intent.putExtra("key",value);
+    /**
+     * 인텐트로 데이터 수신
+     */
+            Intent intent = getIntent();
+            String uri = intent.getStringExtra("key");
 }
