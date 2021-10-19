1f1b03cb1739b4cbd8ce895ec4e4c41b11729706
KimTaeHee <rlaxogmldhkd@gmail.com>
2021-10-19 22:08:52 +0900

    taehee branch first commit

--- app/src/main/java/com/example/everylive/login/Activity_Login.java
+++ app/src/main/java/com/example/everylive/login/Activity_Login.java
@@ -1,8 +1,12 @@
 package com.example.everylive.login;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -21,11 +25,14 @@ import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.Account;
 import com.kakao.sdk.user.model.Gender;
 import com.kakao.sdk.user.model.Profile;
-import com.kakao.sdk.user.model.User;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function2;
@@ -33,13 +40,21 @@ import kotlin.jvm.functions.Function2;
 public class Activity_Login extends AppCompatActivity {
 
     private static final String TAG = "로그인";
-    private ImageView btn_KakaoLogin;
+    private ImageView btn_KakaoLogin, btn_NaverLogin;
+    OAuthLogin mOAuthLoginModule;
+    Context mContext;
+
+    String snsType, nickName, profileIMG, birthday, gender, kakaogender;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         btn_KakaoLogin = findViewById(R.id.btn_KakaoLogin);
+        btn_NaverLogin = findViewById(R.id.btn_NaverLogin);
+
 
 
         /**
@@ -57,7 +72,197 @@ public class Activity_Login extends AppCompatActivity {
             }
         });
 
+
+
+
+        /**
+         * @author 김태희
+         * @brief 네이버 아이디로 로그인
+         * */
+
+        btn_NaverLogin .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 인스턴스를 초기화
+                mOAuthLoginModule = OAuthLogin.getInstance();
+                mOAuthLoginModule.init(
+                        getApplicationContext()
+                        ,getString(R.string.naver_client_id)
+                        ,getString(R.string.naver_client_secret)
+                        ,getString(R.string.naver_client_name)
+
+                );
+
+
+
+                // 로그인창에서 로그인 완료되거나 뒤로가기 눌러서 취소되는 이벤트 받을 핸들러
+                @SuppressLint("HandlerLeak")
+                OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+                    @Override
+                    public void run(boolean success) {
+                        if (success) {
+                            String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                            String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                            long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                            String tokenType = mOAuthLoginModule.getTokenType(mContext);
+
+                            Log.i("LoginData","accessToken : "+ accessToken);
+                            Log.i("LoginData","refreshToken : "+ refreshToken);
+                            Log.i("LoginData","expiresAt : "+ expiresAt);
+                            Log.i("LoginData","tokenType : "+ tokenType);
+
+                            new RequestApiTask(mContext, mOAuthLoginModule).execute();
+
+
+
+
+                        } else {
+                            String errorCode = mOAuthLoginModule
+                                    .getLastErrorCode(mContext).getCode();
+                            String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                            Toast.makeText(getApplicationContext(), "errorCode:" + errorCode
+                                    + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                        }
+                    };
+                };
+
+                // 로그인 시도
+                mOAuthLoginModule.startOauthLoginActivity(Activity_Login.this, mOAuthLoginHandler);
+
+            }
+        });
+
+
+        Button DeleteToken = (Button)findViewById(R.id.DeleteToken);
+        // 네이버 연동해제(클라이언트 및 서버의 토큰 사라짐)
+        DeleteToken .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DeleteTokenTask().execute();
+                Toast.makeText(Activity_Login.this, "연동해제 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button logoutnaver = (Button)findViewById(R.id.logoutnaver);
+        // 네이버 로그아웃(클라이언트에 저장된 토큰 사라짐
+        logoutnaver .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mOAuthLoginModule.logout(mContext);
+                Toast.makeText(Activity_Login.this, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        /**
+         * 네이버 아이디로 로그인
+         * */
+
+
+    } // 온크리에이트
+
+
+    /**
+     * 네이버 아이디로 로그인
+     * */
+    // 네이버 로그인 토큰 연동해제
+    private class DeleteTokenTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            boolean isSuccessDeleteToken = mOAuthLoginModule.logoutAndDeleteToken(mContext);
+
+            if (!isSuccessDeleteToken) {
+                // 서버에서 token 삭제에 실패했어도 클라이언트에 있는 token 은 삭제되어 로그아웃된 상태이다
+                // 실패했어도 클라이언트 상에 token 정보가 없기 때문에 추가적으로 해줄 수 있는 것은 없음
+            }
+
+            return null;
+        }
+
+
     }
+
+
+    // 네이버 로그인 결과값 가져오기
+    public class RequestApiTask extends AsyncTask<Void, Void, String> {
+        private final Context mContext;
+        private final OAuthLogin mOAuthLoginModule;
+        public RequestApiTask(Context mContext, OAuthLogin mOAuthLoginModule) {
+            this.mContext = mContext;
+            this.mOAuthLoginModule = mOAuthLoginModule;
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String url = "https://openapi.naver.com/v1/nid/me";
+            String at = mOAuthLoginModule.getAccessToken(mContext);
+            return mOAuthLoginModule.requestApi(mContext, at, url);
+        }
+
+        protected void onPostExecute(String content) {
+            try {
+                Log.d("로그인한사용자정보", content);
+                JSONObject loginResult = new JSONObject(content);
+                if (loginResult.getString("resultcode").equals("00")){
+                    JSONObject response = loginResult.getJSONObject("response");
+
+
+                    if(response.has("nickname")) {
+                        nickName = response.getString("nickname");
+                        Log.d("사용자정보-닉네임", nickName);
+
+
+                    }
+                    if(response.has("profile_image")) {
+                        profileIMG = response.getString("profile_image");
+                        Log.d("사용자정보-프로필", profileIMG);
+
+
+                    }
+                    if(response.has("gender")) {
+                        gender = response.getString("gender");
+                        Log.d("사용자정보-성별", gender);
+
+
+                    }
+                    if(response.has("birthday")) {
+                        birthday = response.getString("birthday");
+                        Log.d("사용자정보-생일", birthday);
+
+
+                    }
+//                    Toast.makeText(mContext, "email : "+email +" name : "+name+" birthyear : "+birthyear, Toast.LENGTH_LONG).show();
+
+                    // 네이버 로그인할때 체크한 데이터를 Activity_Register에 인텐트로 넘긴다
+                    Intent intent = new Intent(getApplicationContext(), Activity_Register.class);
+
+                    intent.putExtra("birthday",birthday);
+                    intent.putExtra("gender",gender);
+                    intent.putExtra("profileimgURL",profileIMG);
+                    intent.putExtra("nickName",nickName);
+                    intent.putExtra("snsType","naver");
+                    startActivity(intent);
+//                    finish();
+
+
+
+
+
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     * 네이버 아이디로 로그인
+     * */
+
+
 
     // 키해시 얻기
     private String getKeyHash(){
@@ -68,7 +273,7 @@ public class Activity_Login extends AppCompatActivity {
                 try{
                     MessageDigest md = MessageDigest.getInstance("SHA");
                     md.update(signature.toByteArray());
-                    return android.util.Base64.encodeToString(md.digest(), Base64.NO_WRAP);
+                    return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
                 }catch (NoSuchAlgorithmException e){
                     Log.w("getKeyHash", "Unable to get MessageDigest. signature="+signature, e);
                 }
@@ -135,7 +340,7 @@ public class Activity_Login extends AppCompatActivity {
     Function2<OAuthToken, Throwable, Unit> callback = (oAuthToken, throwable) -> {
         if (oAuthToken != null) {
             Log.i("TAG_KAKAO", "성공");
-            Toast.makeText(this, "카카오 로그인이 정상적으로 수행됐습니다.", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "카카오 로그인이 정상적으로 수행됐습니다.", Toast.LENGTH_SHORT).show();
             updateKakaoLogin();
         }
         if (throwable != null) {
@@ -156,6 +361,7 @@ public class Activity_Login extends AppCompatActivity {
                 Account kakaoAccount = user.getKakaoAccount();
                 Profile profile = kakaoAccount.getProfile();
 
+                Log.d(TAG, kakaoAccount.toString());
                 String nickName = profile.getNickname();
                 String profileIMG = profile.getProfileImageUrl(); // 프로필 사진 640*640
                 String thumbnail_image = profile.getThumbnailImageUrl(); // 프로필 사진 110*110
@@ -167,6 +373,26 @@ public class Activity_Login extends AppCompatActivity {
                 Log.i("사용자정보-프로필", profileIMG);
                 Log.i("사용자정보-생일", birthday);
                 Log.i("사용자정보-성별", gender.toString());
+
+                if(gender.toString().equals("MALE")){
+                    kakaogender = "M";
+                }else if(gender.toString().equals("FEMALE")){
+                    kakaogender = "F";
+                 }
+
+                // 카카오 로그인할때 체크한 데이터를 Activity_Register에 인텐트로 넘긴다
+                Intent intent = new Intent(getApplicationContext(), Activity_Register.class);
+
+                intent.putExtra("birthday",birthday);
+                intent.putExtra("gender",kakaogender);
+                intent.putExtra("profileimgURL",profileIMG);
+                intent.putExtra("nickName",nickName);
+                intent.putExtra("snsType","kakao");
+                startActivity(intent);
+//                finish();
+
+
+
 
                 /** DB에서 있는 값인지 체크하고, 없으면 회원가입. 생일과 성별이 null인지 체크하고, 없으면 값을 받는 방식으로? */
             } else {   // 로그인 실패
@@ -180,6 +406,8 @@ public class Activity_Login extends AppCompatActivity {
 //    UserApiClient.getInstance().logout(error -> {
 //        return null;
 //    });
+
+
 
 
 
