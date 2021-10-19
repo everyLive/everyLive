abf895b2bb17c81b472a5afe5d83e462e4431da5
Sumi <charlette1203@gmail.com>
2021-10-19 23:37:00 +0900

    @sumi
    add Activity_Home

---
+++ app/src/main/java/com/example/everylive/home/Activity_Home.java
@@ -0,0 +1,18 @@
+package com.example.everylive.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.everylive.R;
+
+public class Activity_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+    }
+}
